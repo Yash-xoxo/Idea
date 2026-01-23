35faf8e6e7d2d631a74aca93077f842fe26cf78d
Yash-xoxo <sarutofxd@gmail.com>
2026-01-23 12:03:46 +0530

    auto: 50s network capture 2026-01-23_12-03-07

--- net_capture_push.sh
+++ net_capture_push.sh
@@ -35,7 +35,7 @@ echo "[RUN] starting tcpdump"
 
 # -U = packet-buffered (forces write)
 # timeout with KILL fallback guarantees exit
-/usr/bin/timeout -k 5s -s SIGINT 10s \
+/usr/bin/timeout -k 2s -s SIGINT 30s \
   /usr/sbin/tcpdump -i any -nn -XX -U ip \
   > "$OUTFILE" 2>&1
 
